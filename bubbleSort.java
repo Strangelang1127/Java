cab52e9f2b8f7ae2c79e9438bbe30298ed2a6ba2
StrangeLang <84979418+Strangelang1127@users.noreply.github.com>
2023-05-25 15:44:01 +0800

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class bubbleSort {
+	public static void swap(int[] array, int i, int  j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	public static void swap(double[] array, double i, double j) {
+		double temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	public static void main(String[] args) {
+		int[] numbers = {23, 43, 1, 4, 90, 3, 27, 11};
+		boolean swapped;
+		int swappedCounter = 0;
+		 do {
+			 swapped = false;
+			 for(int i = 0; i < numbers.length - 1; i++) {
+				 if(numbers[i] > numbers[i+1]) {
+					 swap(numbers, i,i + 1);
+					 swapped = true;
+					 swappedCounter++;
+				 }
+			 }
+		 }while(swapped);
+		 
+		 for(int i : numbers) {
+			 System.out.print(i+" ");
+		 }
+	}
+}
