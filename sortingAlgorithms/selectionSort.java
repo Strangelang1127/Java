b15b20a8155a1523c821be9838405471120fda8b
StrangeLang <84979418+Strangelang1127@users.noreply.github.com>
2023-05-25 16:13:19 +0800

    Update selectionSort.java

--- sortingAlgorithms/selectionSort.java
+++ sortingAlgorithms/selectionSort.java
@@ -7,16 +7,37 @@ public class selectionSort {
 	public static void selectionSort(int[] arr) {
 		for(int i = 0; i < arr.length - 1; i++) {
 			int minIndex = i;
+			
 			for(int j = i + 1; j < arr.length; j++) {
 				if(arr[j] < arr[minIndex]) {
 					minIndex = j;
 				}
 			}
+			
 			int temp = arr[i];
 			arr[i] = arr[minIndex];
 			arr[minIndex] = temp;
 		}
 		for(int i : arr) {
+			 System.out.print(i+" ");
+		 }
+	}
+	
+	public static void selectionSort(double[] arr) {
+		for(int i = 0; i < arr.length - 1; i++) {
+			int minIndex = i;
+			
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			
+			double temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
+		}
+		for(double i : arr) {
 			 System.out.print(i+" ");
 		 }
 	}
