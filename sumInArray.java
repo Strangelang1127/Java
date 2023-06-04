a2311f64d7f5fcb33e67632bdb7810a0b88b0c8a
StrangeLang <84979418+Strangelang1127@users.noreply.github.com>
2023-06-04 10:34:41 +0800

    Create sumInArray.java

---
+++ sumInArray.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+class sumInArray {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> complementMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(complementMap.containsKey(complement)){
+                return new int[] {complementMap.get(complement), i};
+            }
+            complementMap.put(nums[i], i);
+        }
+          return new int[] {-1, -1};
+    }
+}
